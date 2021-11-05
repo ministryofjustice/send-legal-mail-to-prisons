0d49123bd81d96015bf96b5fbcb6f44a90555189
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-11-05 11:57:44 +0000

    403 Remove system client token - not currently needed

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
 import createApp from './app'
 import HmppsAuthClient from './data/hmppsAuthClient'
-import TokenStore from './data/tokenStore'
 import UserService from './services/userService'
 
-const hmppsAuthClient = new HmppsAuthClient(new TokenStore())
+const hmppsAuthClient = new HmppsAuthClient()
 const userService = new UserService(hmppsAuthClient)
 
 const app = createApp(userService)
