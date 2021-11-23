96243c17a1a9d96cf10c00e046a5e743fa905daf
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2021-11-23 13:43:22 +0000

    * Removed memory store based cookie storage in favour of redis store already setup in `setupWebSession.ts` (#17)
    * Changed alert channels
    * Setup session variable to hold auth token in prep for next PR

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -8,6 +8,7 @@ declare module 'express-session' {
     returnTo: string
     nowInMinutes: number
     requestLinkForm: RequestLinkForm
+    createBarcodeAuthToken: string
   }
 }
 
--- server/middleware/link/setupRequestLink.ts
+++ server/middleware/link/setupRequestLink.ts
@@ -1,27 +1,13 @@
 import express, { Router } from 'express'
-import session from 'express-session'
 import flash from 'connect-flash'
 import RequestLinkController from '../../routes/link/RequestLinkController'
-import config from '../../config'
 import MagicLinkService from '../../services/link/MagicLinkService'
 
 export default function setUpRequestLink(magicLinkService: MagicLinkService): Router {
   const router = express.Router()
   const requestLinksController = new RequestLinkController(magicLinkService)
 
-  // TODO This uses the same max session age and secret as for a logged in user. Consider separating these into different env vars.
-  router.use(
-    session({
-      cookie: { maxAge: config.session.expiryMinutes * 60000 },
-      store: new session.MemoryStore(),
-      saveUninitialized: true,
-      resave: true,
-      secret: config.session.secret,
-    })
-  )
   router.use(flash())
-  router.use(express.json())
-  router.use(express.urlencoded({ extended: true }))
 
   router.get('/request-link', (req, res) => requestLinksController.getRequestLinkView(req, res))
   router.post('/request-link', (req, res) => requestLinksController.submitLinkRequest(req, res))
