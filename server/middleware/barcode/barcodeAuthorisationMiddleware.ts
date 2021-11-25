dcd08ffd2f5d9dfce6a7eab1bade6b502de8c37d
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2021-11-25 11:37:26 +0000

    Removed debug (#24)

--- server/middleware/barcode/barcodeAuthorisationMiddleware.ts
+++ server/middleware/barcode/barcodeAuthorisationMiddleware.ts
@@ -2,14 +2,12 @@ import { RequestHandler } from 'express'
 import type { VerifyErrors, JwtPayload } from 'jsonwebtoken'
 import { verify } from 'jsonwebtoken'
 import config from '../../config'
-import logger from '../../../logger'
 
 export default function barcodeAuthorisationMiddleware(): RequestHandler {
   return (req, res, next) => {
     res.locals.barcodeUser = undefined
 
     if (!req.session.createBarcodeAuthToken) {
-      logger.debug('Barcode auth token is not in the session')
       return res.redirect('/link/request-link')
     }
 
@@ -20,7 +18,6 @@ export default function barcodeAuthorisationMiddleware(): RequestHandler {
       { algorithms: ['RS256'] },
       (err: VerifyErrors, payload: JwtPayload) => {
         if (err) {
-          logger.debug('Could not verify the sig of the Barcode auth token')
           req.flash('errors', [{ text: 'The link you used is no longer valid. Request a new one to log in.' }])
           return res.redirect('/link/request-link')
         }
