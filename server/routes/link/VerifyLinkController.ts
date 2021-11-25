55a97a692e76581d9e3fb921ab957d4a58b49923
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2021-11-25 11:29:03 +0000

    Removed debug

--- server/routes/link/VerifyLinkController.ts
+++ server/routes/link/VerifyLinkController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express'
 import MagicLinkService from '../../services/link/MagicLinkService'
-import logger from '../../../logger'
 
 export default class VerifyLinkController {
   constructor(private readonly magicLinkService: MagicLinkService) {}
@@ -8,7 +7,6 @@ export default class VerifyLinkController {
   async verifyLink(req: Request, res: Response): Promise<void> {
     const secret = req.query.secret as string
     if (!secret) {
-      logger.debug('No secret on the query string')
       return res.redirect('request-link')
     }
 
@@ -16,11 +14,9 @@ export default class VerifyLinkController {
       .verifyLink(secret)
       .then(token => {
         req.session.createBarcodeAuthToken = token
-        logger.debug('added barcode auth token to session - about to redirect')
         res.redirect('/barcode/find-recipient')
       })
       .catch(() => {
-        logger.debug('Unable to return token from API - probably been used before')
         req.flash('errors', [{ text: 'The link you used is no longer valid. Request a new one to log in.' }])
         res.redirect('request-link')
       })
