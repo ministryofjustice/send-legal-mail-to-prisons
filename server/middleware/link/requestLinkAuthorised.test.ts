9c9ab38d7c50d7e69d6f5aea16de79cca47b814a
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-12-03 08:49:39 +0000

    SLM-14 - Add unit test for /link/request-link redirect

---
+++ server/middleware/link/requestLinkAuthorised.test.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express'
+import requestLinkAuthorised from './requestLinkAuthorised'
+
+const req = { session: {} } as Request
+const res = { redirect: jest.fn() } as unknown as Response
+const next = jest.fn()
+
+const middleware = requestLinkAuthorised()
+
+describe('redirect signed in users to the app if they request /link/request-link', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should do nothing if not requesting the request-link page', async () => {
+    req.originalUrl = '/something/else'
+
+    await middleware(req, res, next)
+
+    expect(res.redirect).not.toBeCalled()
+    expect(next).toBeCalledTimes(1)
+  })
+
+  it('should redirect if already signed in', async () => {
+    req.originalUrl = '/link/request-link'
+    req.session.validCreateBarcodeAuthToken = true
+
+    await middleware(req, res, next)
+
+    expect(res.redirect).toBeCalledTimes(1)
+    expect(next).not.toBeCalled()
+  })
+
+  it('should not redirect if not signed in', async () => {
+    req.originalUrl = '/link/request-link'
+    req.session.validCreateBarcodeAuthToken = false
+
+    await middleware(req, res, next)
+
+    expect(res.redirect).not.toBeCalled()
+    expect(next).toBeCalledTimes(1)
+  })
+
+  it('should not redirect if forcing a sign out', async () => {
+    req.originalUrl = '/link/request-link?force=true'
+    req.session.validCreateBarcodeAuthToken = true
+
+    await middleware(req, res, next)
+
+    expect(res.redirect).not.toBeCalled()
+    expect(next).toBeCalledTimes(1)
+  })
+})
