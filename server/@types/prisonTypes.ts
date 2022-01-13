86a0fe0ecbaf87e8b3844bc05181c99164c9bb5e
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-01-13 13:36:29 +0000

    SLM-75: Service method to get prison address (#84)

--- server/@types/prisonTypes.ts
+++ server/@types/prisonTypes.ts
@@ -2,3 +2,13 @@ export type Prison = {
   id: string
   name: string
 }
+
+export type PrisonAddress = {
+  flat?: string
+  premise?: string
+  street?: string
+  locality?: string
+  countyCode?: string
+  area?: string
+  postalCode?: string
+}
