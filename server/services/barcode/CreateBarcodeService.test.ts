303299d85e2fe0832cdb26f5242f3ebc71991ec1
Mike Halma <mike.halma@digital.justice.gov.uk>
2022-01-16 16:34:50 +0000

    SLM-95 Add missing test for calling createBarcode API

--- server/services/barcode/CreateBarcodeService.test.ts
+++ server/services/barcode/CreateBarcodeService.test.ts
@@ -1,16 +1,34 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { toBuffer } from 'bwip-js'
+import nock from 'nock'
 import CreateBarcodeService from './CreateBarcodeService'
+import config from '../../config'
 
 jest.mock('bwip-js')
 const mockBwipjsToBuffer = <jest.Mock<typeof toBuffer>>toBuffer
 
 describe('CreateBarcodeService', () => {
   let createBarcodeService: CreateBarcodeService
+  let mockedSendLegalMailApi: nock.Scope
 
   beforeEach(() => {
+    mockedSendLegalMailApi = nock(config.apis.sendLegalMail.url)
     createBarcodeService = new CreateBarcodeService()
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  describe('createBarcode', () => {
+    it('should call the create barcoe API', async () => {
+      mockedSendLegalMailApi.post('/barcode').reply(201, { barcode: '123456789012' })
+
+      createBarcodeService.createBarcode('some-token').then(barcode => {
+        expect(barcode).toEqual('123456789012')
+      })
+    })
   })
 
   describe('generateBarcodeImage', () => {
@@ -19,10 +37,10 @@ describe('CreateBarcodeService', () => {
     })
 
     it('should pass the barcode into the barcode generator', async () => {
-      const response = await createBarcodeService.generateBarcodeImage('somebarcode1')
+      await createBarcodeService.generateBarcodeImage('123456789012')
 
       expect(mockBwipjsToBuffer).toHaveBeenCalledWith(
-        expect.objectContaining({ text: 'somebarcode1', alttext: 'some-barc-ode1' })
+        expect.objectContaining({ text: '123456789012', alttext: '1234-5678-9012' })
       )
     })
   })
