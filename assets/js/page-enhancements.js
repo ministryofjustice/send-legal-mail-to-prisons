3ceb6eb934a939ccf0062ead628b8156c0de3284
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-01-20 08:17:13 +0000

    SLM-94: Fix prison name autocomplete when either blank or non-matching prison name is selected

--- assets/js/page-enhancements.js
+++ assets/js/page-enhancements.js
@@ -30,6 +30,24 @@
         selectElement: element,
       })
     })
+    $('#create-new-contact-form').submit(event => {
+      // Before submitting the form set the prisonId select option to the value matching the autocomplete, or the null option if there is no match
+      // This is because the autocomplete currently only sets the select option when there is a valid match, which causes incorrect
+      // behaviour when the user performs a valid autocomplete, then changes it to blank or non-matching text. In this case the underlying
+      // select is not reset so we have to do it ourselves.
+      event.preventDefault()
+      const underlyingSelectField = $('#prisonId-select')
+      const autoCompleteField = $('#prisonId')
+      // In all cases start off by setting the underlying select option to the null/empty option
+      underlyingSelectField.children(`option[value='']`).prop('selected', true)
+      // Now find the option whose text matches the autocomplete field, and select it. If there is no match nothing will be selected
+      underlyingSelectField
+        .children('option')
+        .filter((idx, option) => option.text === autoCompleteField.val())
+        .prop('selected', true)
+      // Submit the form
+      event.target.submit()
+    })
   })
 })($, document)
 ;(enableCopyBarcodeButton = ($, document) => {
--- integration_tests/integration/barcode/createNewContact.spec.ts
+++ integration_tests/integration/barcode/createNewContact.spec.ts
@@ -1,6 +1,7 @@
 import FindRecipientByPrisonNumberPage from '../../pages/barcode/findRecipientByPrisonNumber'
 import Page from '../../pages/page'
 import ReviewRecipientsPage from '../../pages/barcode/reviewRecipients'
+import CreateNewContactPage from '../../pages/barcode/createNewContact'
 
 context('Create New Contact Page', () => {
   it('should redirect to find-recipient given user navigates to Create New Contact without going via find-recipients first', () => {
@@ -16,5 +17,29 @@ context('Create New Contact Page', () => {
     createNewContactPage.submitWithValidValues()
 
     Page.verifyOnPage(ReviewRecipientsPage)
+  })
+
+  it('should redisplay create-new-contact given form submitted with prison that was previously correctly selected but an invalid prison typed in', () => {
+    const createNewContactPage = FindRecipientByPrisonNumberPage.goToPage().submitWithValidPrisonNumber()
+
+    createNewContactPage
+      .enterAValidPrisonerName()
+      .typeAheadAValidPrison()
+      .typeAheadAnInvalidPrison()
+      .submitForm(CreateNewContactPage)
+
+    Page.verifyOnPage(CreateNewContactPage).hasPrisonIdErrorContaining('prison name')
+  })
+
+  it('should redisplay create-new-contact given form submitted with prison that was previously correctly selected but then blanked out', () => {
+    const createNewContactPage = FindRecipientByPrisonNumberPage.goToPage().submitWithValidPrisonNumber()
+
+    createNewContactPage
+      .enterAValidPrisonerName()
+      .typeAheadAValidPrison()
+      .clearPrisonField()
+      .submitForm(CreateNewContactPage)
+
+    Page.verifyOnPage(CreateNewContactPage).hasPrisonIdErrorContaining('prison name')
   })
 })
