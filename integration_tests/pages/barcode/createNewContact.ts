44d892d8175c47cec495c0040fd61037a30043c4
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-01-20 08:52:17 +0000

    SLM-94: Fix prison name autocomplete when either blank or non-matching prison name is selected (#102)

--- integration_tests/pages/barcode/createNewContact.ts
+++ integration_tests/pages/barcode/createNewContact.ts
@@ -14,15 +14,53 @@ export default class CreateNewContactPage extends Page {
   }
 
   submitWithValidValues = (): ReviewRecipientsPage => {
+    this.enterAValidPrisonerName()
+    this.typeAheadAValidPrison()
+    return this.submitForm(ReviewRecipientsPage)
+  }
+
+  enterAValidPrisonerName = (): CreateNewContactPage => {
+    this.prisonerNameField().clear()
     this.prisonerNameField().type('Gage Hewitt')
+    return this
+  }
+
+  typeAheadAValidPrison = (): CreateNewContactPage => {
+    this.clearPrisonField()
     this.prisonIdField().type('ashfield')
     this.pressEnterInPrisonIdField()
-    this.submitButton().click()
-    return Page.verifyOnPage(ReviewRecipientsPage)
+    return this
+  }
+
+  typeAheadAnInvalidPrison = (): CreateNewContactPage => {
+    this.clearPrisonField()
+    this.prisonIdField().type('not a valid prison')
+    this.pressEnterInPrisonIdField()
+    return this
+  }
+
+  clearPrisonField = (): CreateNewContactPage => {
+    this.prisonIdField().clear()
+    return this
   }
 
   pressEnterInPrisonIdField = () => {
     this.prisonIdField().type('\n')
+  }
+
+  submitForm<T>(T): T {
+    this.submitButton().click()
+    return Page.verifyOnPage(T)
+  }
+
+  hasPrisonerNameErrorContaining = (partialMessage: string): void => {
+    cy.get('.govuk-error-summary__list').should('contain', partialMessage)
+    cy.get('#prisonerName-error').should('contain', partialMessage)
+  }
+
+  hasPrisonIdErrorContaining = (partialMessage: string): void => {
+    cy.get('.govuk-error-summary__list').should('contain', partialMessage)
+    cy.get('#prisonId-error').should('contain', partialMessage)
   }
 
   prisonerNameField = (): PageElement => cy.get('#prisonerName')
