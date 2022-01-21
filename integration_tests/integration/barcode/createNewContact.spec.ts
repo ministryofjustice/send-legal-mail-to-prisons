1dc84ecf3f5c00e7d4f2476c93fd596020db0d02
Mike Halma <mike.halma@digital.justice.gov.uk>
2022-01-21 10:27:16 +0000

    SLM-110 Remove extraneous only

--- integration_tests/integration/barcode/createNewContact.spec.ts
+++ integration_tests/integration/barcode/createNewContact.spec.ts
@@ -19,7 +19,7 @@ context('Create New Contact Page', () => {
     Page.verifyOnPage(ReviewRecipientsPage)
   })
 
-  it.only('should find a prison when searching with HMP prefix', () => {
+  it('should find a prison when searching with HMP prefix', () => {
     const createNewContactPage = FindRecipientByPrisonNumberPage.goToPage().submitWithValidPrisonNumber()
 
     createNewContactPage.enterAValidPrisonerName().typeAheadHMPValidPrison().submitForm(ReviewRecipientsPage)
