0f7ed92533f7350f4c0dc8fbdf9006fe6a9cbab7
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-01-25 19:00:53 +0000

    SLM-81 Add integration tests (#125)
    * SLM-81 Use prisoner DOB if no prison number
    
    * SLM-81 Use prisoner DOB if no prison number
    
    * SLM-81 Add integration test for recipient without prison number
    
    * SLM-81 Include a prisoner by DOB in the legal sender e2e test

---
+++ integration_tests/pages/barcode/findRecipientByPrisonerName.ts
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-cycle */
+import Page, { PageElement } from '../page'
+import FindRecipientByPrisonNumberPage from './findRecipientByPrisonNumber'
+import CreateNewContactByPrisonerNamePage from './createNewContactByPrisonerName'
+
+export default class FindRecipientByPrisonerNamePage extends Page {
+  constructor() {
+    super('find-recipient-by-prisoner-name')
+    this.prisonerNameFieldIsFocussed()
+  }
+
+  prisonerNameFieldIsFocussed = () => {
+    this.prisonerNameField().should('be.focused')
+  }
+
+  submitWithValidPrisonerName = (prisonerName = 'John Smith'): CreateNewContactByPrisonerNamePage => {
+    this.prisonerNameField().type(prisonerName)
+    this.submitButton().click()
+    return Page.verifyOnPage(CreateNewContactByPrisonerNamePage)
+  }
+
+  submitWithInvalidPrisonerName = (): FindRecipientByPrisonerNamePage => {
+    this.prisonerNameField().type(' ')
+    this.submitButton().click()
+    return Page.verifyOnPage(FindRecipientByPrisonerNamePage)
+  }
+
+  prisonerNameField = (): PageElement => cy.get('#prisonerName')
+
+  submitButton = (): PageElement => cy.get('#find-recipient-by-prisoner-name-form button')
+
+  hasErrorContaining = (partialMessage: string): void => {
+    cy.get('.govuk-error-summary__list').should('contain', partialMessage)
+    cy.get('#prisonerName-error').should('contain', partialMessage)
+  }
+
+  static goToPage = (): FindRecipientByPrisonerNamePage =>
+    FindRecipientByPrisonNumberPage.goToPage().goToByPrisonerName()
+}
--- server/filters/recipientTableRowsFilter.ts
+++ server/filters/recipientTableRowsFilter.ts
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import { Recipient } from '../@types/prisonTypes'
 
 type TableCell = {
@@ -9,7 +10,9 @@ export default function recipientTableRowsFilter(recipients: Array<Recipient>): 
   return recipients.map((recipient, idx) => {
     return Array.of(
       { text: recipient.prisonerName }, // Column 1 - Prisoner Name
-      { text: recipient.prisonNumber }, // Column 2 - Prison number or DOB - TODO SLM-81
+      {
+        text: recipient.prisonNumber ? recipient.prisonNumber : moment(recipient.prisonerDob).format('DD-MM-YYYY'), // Column 2 - Prison number or DOB
+      },
       { text: recipient.prisonAddress.premise }, // Column 3 - Prison name
       { html: '<a href="" class="govuk-link">Edit details</a>' }, // Column 4 - Edit recipient details link - TODO SLM-63
       { html: `<a href="/barcode/review-recipients/remove/${idx}" class="govuk-link">Remove</a>` } // Column 5 - Remove recipient link
