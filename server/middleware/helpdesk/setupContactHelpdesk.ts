cb2b75f82a87445394e2092a3898b68053743f6f
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-02-08 11:32:21 +0000

    SLM-106: Defined const for max file size

--- server/middleware/helpdesk/setupContactHelpdesk.ts
+++ server/middleware/helpdesk/setupContactHelpdesk.ts
@@ -1,12 +1,13 @@
 import express, { Router } from 'express'
 import multer from 'multer'
 import ContactHelpdeskController from '../../routes/helpdesk/ContactHelpdeskController'
+import { MAX_FILE_SIZE } from '../../routes/helpdesk/validators/uploadValidator'
 
 export default function setupContactHelpdesk(): Router {
   const router = express.Router()
   const contactHelpdeskController = new ContactHelpdeskController()
 
-  const upload = multer({ dest: 'uploads/', limits: { files: 1, fileSize: 20 * 1024 * 1024 } })
+  const upload = multer({ dest: 'uploads/', limits: { files: 1, fileSize: MAX_FILE_SIZE } })
 
   router.get('/', (req, res) => contactHelpdeskController.getContactHelpdeskView(req, res))
   router.post('/', upload.single('screenshot'), (req, res) => contactHelpdeskController.submitContactHelpdesk(req, res))
--- server/routes/helpdesk/validators/uploadValidator.ts
+++ server/routes/helpdesk/validators/uploadValidator.ts
@@ -1,4 +1,5 @@
-const MAX_FILE_SIZE = 20 * 1024 * 1024
+export const MAX_FILE_SIZE = 20 * 1024 * 1024
+
 const SUPPORTED_MIME_TYPES = ['image/jpg', 'image/jpeg', 'image/bmp', 'image/png', 'image/tif', 'application/pdf']
 
 export default function validateUpload(upload: Express.Multer.File): Array<string> {
