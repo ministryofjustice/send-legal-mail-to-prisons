77bec25d601fe599b16307f8e24d8f3f15d97555
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-02-08 11:26:11 +0000

    SLM-106: Allow contact helpdesk form to post a file attachment as a multipart form

--- server/middleware/helpdesk/setupContactHelpdesk.ts
+++ server/middleware/helpdesk/setupContactHelpdesk.ts
@@ -1,12 +1,15 @@
 import express, { Router } from 'express'
+import multer from 'multer'
 import ContactHelpdeskController from '../../routes/helpdesk/ContactHelpdeskController'
 
 export default function setupContactHelpdesk(): Router {
   const router = express.Router()
   const contactHelpdeskController = new ContactHelpdeskController()
 
+  const upload = multer({ dest: 'uploads/', limits: { files: 1, fileSize: 20 * 1024 * 1024 } })
+
   router.get('/', (req, res) => contactHelpdeskController.getContactHelpdeskView(req, res))
-  router.post('/', (req, res) => contactHelpdeskController.submitContactHelpdesk(req, res))
+  router.post('/', upload.single('screenshot'), (req, res) => contactHelpdeskController.submitContactHelpdesk(req, res))
   router.get('/submitted', (req, res) => contactHelpdeskController.getContactHelpdeskSubmittedView(req, res))
 
   return router
---
+++ server/routes/helpdesk/validators/uploadValidator.test.ts
@@ -0,0 +1,69 @@
+import validateUpload from './uploadValidator'
+
+describe('uploadValidator', () => {
+  it(`should validate given no uploaded file`, () => {
+    const upload: Express.Multer.File = null
+
+    const errors = validateUpload(upload)
+
+    expect(errors).toStrictEqual([])
+  })
+
+  it(`should validate given a valid uploaded file`, () => {
+    const upload = {
+      size: 1,
+      mimetype: 'image/jpg',
+    } as Express.Multer.File
+
+    const errors = validateUpload(upload)
+
+    expect(errors).toStrictEqual([])
+  })
+
+  it(`should not validate given an empty uploaded file`, () => {
+    const upload = {
+      size: 0,
+      mimetype: 'image/jpg',
+    } as Express.Multer.File
+
+    const errors = validateUpload(upload)
+
+    expect(errors).toStrictEqual(['The selected file cannot be empty'])
+  })
+
+  it(`should not validate given an uploaded file that is too big`, () => {
+    const upload = {
+      size: 20 * 1024 * 1024 + 1,
+      mimetype: 'image/jpg',
+    } as Express.Multer.File
+
+    const errors = validateUpload(upload)
+
+    expect(errors).toStrictEqual(['The selected file must be smaller than 20MB'])
+  })
+
+  it(`should not validate given an unsupported file type`, () => {
+    const upload = {
+      size: 20,
+      mimetype: 'application/octet-stream',
+    } as Express.Multer.File
+
+    const errors = validateUpload(upload)
+
+    expect(errors).toStrictEqual(['The selected file must be a JPG, JPEG, BMP, PNG, TIF, or PDF'])
+  })
+
+  it(`should not validate given an unsupported file type that is also too large`, () => {
+    const upload = {
+      size: 20 * 1024 * 1024 + 1,
+      mimetype: 'application/octet-stream',
+    } as Express.Multer.File
+
+    const errors = validateUpload(upload)
+
+    expect(errors).toStrictEqual([
+      'The selected file must be smaller than 20MB',
+      'The selected file must be a JPG, JPEG, BMP, PNG, TIF, or PDF',
+    ])
+  })
+})
