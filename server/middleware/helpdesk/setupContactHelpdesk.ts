edaa07e883d55337dff86ed198020814ce83ca56
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-02-08 13:16:57 +0000

    SLM-106: Enable file upload (#150)
    SLM-106: Allow contact helpdesk form to post a file attachment as a multipart form

--- server/middleware/helpdesk/setupContactHelpdesk.ts
+++ server/middleware/helpdesk/setupContactHelpdesk.ts
@@ -1,12 +1,16 @@
 import express, { Router } from 'express'
+import multer from 'multer'
 import ContactHelpdeskController from '../../routes/helpdesk/ContactHelpdeskController'
+import { MAX_FILE_SIZE } from '../../routes/helpdesk/validators/uploadValidator'
 
 export default function setupContactHelpdesk(): Router {
   const router = express.Router()
   const contactHelpdeskController = new ContactHelpdeskController()
 
+  const upload = multer({ dest: 'uploads/', limits: { files: 1, fileSize: MAX_FILE_SIZE } })
+
   router.get('/', (req, res) => contactHelpdeskController.getContactHelpdeskView(req, res))
-  router.post('/', (req, res) => contactHelpdeskController.submitContactHelpdesk(req, res))
+  router.post('/', upload.single('screenshot'), (req, res) => contactHelpdeskController.submitContactHelpdesk(req, res))
   router.get('/submitted', (req, res) => contactHelpdeskController.getContactHelpdeskSubmittedView(req, res))
 
   return router
--- server/routes/helpdesk/ContactHelpdeskController.ts
+++ server/routes/helpdesk/ContactHelpdeskController.ts
@@ -1,7 +1,9 @@
+import fs from 'fs'
 import { Request, Response } from 'express'
 import type { ContactHelpdeskForm } from 'forms'
 import ContactHelpdeskView from './ContactHelpdeskView'
 import validate from './contactHelpdeskFormValidator'
+import logger from '../../../logger'
 
 export default class ContactHelpdeskController {
   async getContactHelpdeskView(req: Request, res: Response): Promise<void> {
@@ -15,7 +17,7 @@ export default class ContactHelpdeskController {
 
   async submitContactHelpdesk(req: Request, res: Response): Promise<void> {
     req.session.contactHelpdeskForm = { ...req.body }
-    const errors = validate(req.session.contactHelpdeskForm)
+    const errors = validate(req.session.contactHelpdeskForm, req.file)
     if (errors.length > 0) {
       req.flash('errors', errors)
       // Redirect to original url inc. query string because Legal Sender and Mail Room journeys have different urls, so can't hardcode a redirect url here
@@ -23,6 +25,11 @@ export default class ContactHelpdeskController {
     }
 
     // TODO - SLM-126 - call service to send data to zendesk
+    // Uploaded file properties are available in req.file; remember to delete req.file.path
+    if (req.file) {
+      logger.info(`Uploaded file: ${JSON.stringify(req.file)}`)
+      fs.unlinkSync(req.file.path)
+    }
 
     req.session.contactHelpdeskForm = undefined
     // Use request baseUrl as the basis for the redirect because Legal Sender and Mail Room journeys have different urls, so can't hardcode a redirect url here
