acc85cc7021f812c4fccc607e5c4ab36f67b9cb5
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-02-11 09:19:06 +0000

    SLM-20 Need lax cookie sameSite policy for email links (#158)
    If you set the cookie sameSite policy to strict then when you click on the email link to sign in via CJSM it doesn't pull cookies through - this means the session and cookie policy are lost and the email link doesn't work.
    
    Needs a little more investigation to make sure this works when opening the link in a different browser.

--- server/routes/cookies/CookiesPolicyController.ts
+++ server/routes/cookies/CookiesPolicyController.ts
@@ -39,7 +39,7 @@ export default class CookiesPolicyController {
       return res
         .cookie('cookies_policy', req.body.cookies, {
           maxAge: 365 * 24 * 60 * 60 * 1000,
-          sameSite: 'strict',
+          sameSite: 'lax',
           secure: true,
         })
         .redirect(`${redirectUrl}?showCookieConfirmation=true`)
