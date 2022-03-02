af4391a50d160c71ad0f856490c668d1930d57f0
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2022-03-02 08:34:25 +0000

    SLM-167 session fixation (#206)
    * SLM-167 Generate a new sessing after signing in
    
    * SLM-167 All cookies should be httpOnly
    
    * SLM-167 Add unit tests for verify link
    
    * SLM-167 Change back to prod value!
    
    * SLM-167 Tidy up some tests

--- server/middleware/barcode/barcodeAuthorisationMiddleware.ts
+++ server/middleware/barcode/barcodeAuthorisationMiddleware.ts
@@ -1,36 +1,13 @@
 import { RequestHandler } from 'express'
-import type { VerifyErrors, JwtPayload } from 'jsonwebtoken'
-import { verify } from 'jsonwebtoken'
-import config from '../../config'
 
 export default function barcodeAuthorisationMiddleware(): RequestHandler {
   return (req, res, next) => {
     res.locals.barcodeUser = undefined
 
-    if (!req.session.slmToken) {
+    if (!req.session.slmToken || !req.session.validSlmToken) {
       return res.redirect('/link/request-link')
     }
 
-    const token = req.session.slmToken
-    req.session.validSlmToken = false
-    return verify(
-      token,
-      config.barcodeTokenPublicKey,
-      { algorithms: ['RS256'] },
-      (err: VerifyErrors, payload: JwtPayload) => {
-        if (err) {
-          req.flash('errors', [
-            { href: '#email', text: 'The link you used is no longer valid. Request a new one to sign in.' },
-          ])
-          return res.redirect('/link/request-link')
-        }
-        req.session.validSlmToken = true
-        req.session.barcodeUserEmail = payload.sub
-        req.session.barcodeUserOrganisation = payload.organisation
-        // make the session expiry the same as the JWT - otherwise we lose the JWT when the session expires
-        req.session.cookie.expires = new Date(payload.exp * 1000)
-        return next()
-      }
-    )
+    return next()
   }
 }
---
+++ server/routes/link/VerifLinkController.test.ts
@@ -0,0 +1,149 @@
+import { Request, Response } from 'express'
+import { JsonWebTokenError, verify } from 'jsonwebtoken'
+import VerifyLinkController from './VerifyLinkController'
+import MagicLinkService from '../../services/link/MagicLinkService'
+
+jest.mock('jsonwebtoken')
+
+const req = {
+  session: {
+    validSlmToken: false,
+    slmToken: undefined as string,
+    barcodeUserEmail: undefined as string,
+    barcodeUserOrganisation: undefined as string,
+    cookie: {
+      expires: undefined as Date,
+    },
+    regenerate: jest.fn(),
+  },
+  flash: jest.fn(),
+  body: {},
+  query: { secret: undefined as string },
+}
+const res = {
+  render: jest.fn(),
+  redirect: jest.fn(),
+}
+const magicLinkService = {
+  verifyLink: jest.fn(),
+}
+
+describe('VerifyLinkController', () => {
+  let verifyLinkController: VerifyLinkController
+  let mockVerify: jest.MockedFunction<typeof verify>
+
+  beforeEach(() => {
+    verifyLinkController = new VerifyLinkController(magicLinkService as unknown as MagicLinkService)
+    mockVerify = verify as jest.MockedFunction<typeof verify>
+  })
+
+  afterEach(() => {
+    req.session.validSlmToken = false
+    req.session.slmToken = undefined as string
+    req.session.barcodeUserEmail = undefined as string
+    req.session.barcodeUserOrganisation = undefined as string
+    req.session.cookie = {
+      expires: undefined as Date,
+    }
+    req.query.secret = undefined as string
+    jest.resetAllMocks()
+  })
+
+  describe('verifyLink - sad path', () => {
+    it('should redirect if no secret passed', async () => {
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(res.redirect).toHaveBeenCalledWith('request-link')
+    })
+
+    it('should show error and redirect if the magic link service fails', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockRejectedValue('some-error')
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(req.flash).toHaveBeenCalledWith(
+        'errors',
+        expect.arrayContaining([expect.objectContaining({ href: '#email' })])
+      )
+      expect(res.redirect).toHaveBeenCalledWith('request-link')
+    })
+
+    it('should show error and redirect if token verification fails', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockResolvedValue('some-token')
+      mockVerify.mockImplementation((token, key, opts, verifyCallback) => {
+        verifyCallback(new JsonWebTokenError('some-error'), undefined)
+      })
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(req.flash).toHaveBeenCalledWith(
+        'errors',
+        expect.arrayContaining([expect.objectContaining({ href: '#email' })])
+      )
+      expect(res.redirect).toHaveBeenCalledWith('/link/request-link')
+    })
+  })
+
+  describe('verifyLink - happy path', () => {
+    it('should set the token on the session', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockResolvedValue('some-token')
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(magicLinkService.verifyLink).toHaveBeenCalledWith('some-secret')
+      expect(req.session.slmToken).toStrictEqual('some-token')
+    })
+
+    it('should verify the token on the session', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockResolvedValue('some-token')
+      mockVerify.mockImplementation((token, key, opts, verifyCallback) => {
+        verifyCallback(undefined, {})
+      })
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(req.session.validSlmToken).toStrictEqual(true)
+    })
+
+    it('should set token details on the session', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockResolvedValue('some-token')
+      mockVerify.mockImplementation((token, key, opts, verifyCallback) => {
+        verifyCallback(undefined, { sub: 'some-email', organisation: 'some-org' })
+      })
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(req.session.barcodeUserEmail).toStrictEqual('some-email')
+      expect(req.session.barcodeUserOrganisation).toStrictEqual('some-org')
+    })
+
+    it('should set cookie expiry from the token', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockResolvedValue('some-token')
+      mockVerify.mockImplementation((token, key, opts, verifyCallback) => {
+        verifyCallback(undefined, { exp: 1646155014 })
+      })
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(req.session.cookie.expires).toStrictEqual(new Date(2022, 2, 1, 17, 16, 54))
+    })
+
+    it('should regenerate the session', async () => {
+      req.query.secret = 'some-secret'
+      magicLinkService.verifyLink.mockResolvedValue('some-token')
+      mockVerify.mockImplementation((token, key, opts, verifyCallback) => {
+        verifyCallback(undefined, {})
+      })
+
+      await verifyLinkController.verifyLink(req as unknown as Request, res as unknown as Response)
+
+      expect(req.session.regenerate).toHaveBeenCalled()
+    })
+  })
+})
