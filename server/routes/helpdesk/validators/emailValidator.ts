752c147f180add3fbaf6e66c94e797fe531699c5
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-03-04 15:23:57 +0000

    SLM-191: Email hint text and validation for Legal Senders on Contact Helpdesk page (#219)

--- server/routes/helpdesk/validators/emailValidator.ts
+++ server/routes/helpdesk/validators/emailValidator.ts
@@ -1,5 +1,6 @@
 const EMAIL_PATTERN =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const CJSM_NET_EMAIL = /^.+[@.]cjsm\.net$/i
 
 export default function validateEmail(email?: string): Array<string> {
   const errors: Array<string> = []
@@ -8,6 +9,8 @@ export default function validateEmail(email?: string): Array<string> {
     errors.push('Enter an email address')
   } else if (!EMAIL_PATTERN.test(email)) {
     errors.push('Enter an email address in the correct format')
+  } else if (CJSM_NET_EMAIL.test(email)) {
+    errors.push(`Enter an email address which does not end 'cjsm.net'`)
   }
 
   return errors
