aed99630d55071754367adf9dcb902117ce5530c
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-04-11 16:18:18 +0100

    SLM-231: Fleshed out service call to submit email to request a one time code (#276)

---
+++ server/routes/one-time-code-auth/RequestOneTimeCodeController.test.ts
@@ -0,0 +1,141 @@
+import { Request, Response } from 'express'
+import type { SessionData } from 'express-session'
+import RequestOneTimeCodeController from './RequestOneTimeCodeController'
+import VerifyOneTimeCodeController from './VerifyOneTimeCodeController'
+import OneTimeCodeService from '../../services/one-time-code-auth/OneTimeCodeService'
+import config from '../../config'
+import validate from './RequestOneTimeCodeValidator'
+
+const req = {
+  session: {} as SessionData,
+  flash: jest.fn(),
+  body: {},
+  ip: '127.0.0.1',
+  sessionID: '12345678',
+}
+const res = {
+  render: jest.fn(),
+  redirect: jest.fn(),
+}
+
+const oneTimeCodeService = {
+  requestOneTimeCode: jest.fn(),
+}
+const verifyOneTimeCodeController = {
+  verifyOneTimeCode: jest.fn(),
+}
+
+jest.mock('../../config')
+jest.mock('./RequestOneTimeCodeValidator')
+
+describe('RequestOneTimeCodeController', () => {
+  const requestOneTimeCodeController = new RequestOneTimeCodeController(
+    oneTimeCodeService as unknown as OneTimeCodeService,
+    verifyOneTimeCodeController as unknown as VerifyOneTimeCodeController
+  )
+
+  beforeEach(() => {
+    config.oneTimeCodeValidityDuration = 10
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  describe('getRequestOneTimeCodeView', () => {
+    it('should render view given no requestOneTimeCodeForm on the session', async () => {
+      req.session.requestOneTimeCodeForm = undefined
+
+      await requestOneTimeCodeController.getRequestOneTimeCodeView(
+        req as undefined as Request,
+        res as undefined as Response
+      )
+
+      expect(res.render).toHaveBeenCalledWith('pages/one-time-code-auth/requestOneTimeCode', {
+        errors: [],
+        form: {},
+        oneTimeCodeValidityDuration: 10,
+      })
+      expect(req.session.requestOneTimeCodeForm).toBeUndefined()
+    })
+
+    it('should render view given requestOneTimeCodeForm on the session and errors', async () => {
+      req.session.requestOneTimeCodeForm = { email: 'someone@aarvark.com' }
+      req.flash.mockReturnValue([{ href: '#email', text: 'Enter a valid email' }])
+
+      await requestOneTimeCodeController.getRequestOneTimeCodeView(
+        req as undefined as Request,
+        res as undefined as Response
+      )
+
+      expect(res.render).toHaveBeenCalledWith('pages/one-time-code-auth/requestOneTimeCode', {
+        errors: [{ href: '#email', text: 'Enter a valid email' }],
+        form: { email: 'someone@aarvark.com' },
+        oneTimeCodeValidityDuration: 10,
+      })
+      expect(req.session.requestOneTimeCodeForm).toBeUndefined()
+    })
+  })
+
+  describe('submitOneTimeCodeRequest', () => {
+    const mockRequestOneTimeCodeValidator = validate as jest.MockedFunction<typeof validate>
+
+    it('should redirect to email sent page given valid requestOneTimeCodeForm', async () => {
+      req.body = { email: 'someone@aarvark.com.cjsm.net' }
+      mockRequestOneTimeCodeValidator.mockReturnValue(true)
+      oneTimeCodeService.requestOneTimeCode.mockResolvedValue(undefined)
+
+      await requestOneTimeCodeController.submitOneTimeCodeRequest(
+        req as undefined as Request,
+        res as undefined as Response
+      )
+
+      expect(oneTimeCodeService.requestOneTimeCode).toHaveBeenCalledWith(
+        'someone@aarvark.com.cjsm.net',
+        '12345678',
+        '127.0.0.1'
+      )
+      expect(res.redirect).toHaveBeenCalledWith('email-sent')
+      expect(req.session.lsjSmokeTestUser).toBeFalsy()
+    })
+
+    it('should redirect to request code page given invalid requestOneTimeCodeForm', async () => {
+      req.body = { email: 'someone@aarvark.com' }
+      mockRequestOneTimeCodeValidator.mockReturnValue(false)
+
+      await requestOneTimeCodeController.submitOneTimeCodeRequest(
+        req as undefined as Request,
+        res as undefined as Response
+      )
+
+      expect(oneTimeCodeService.requestOneTimeCode).not.toHaveBeenCalled()
+      expect(res.redirect).toHaveBeenCalledWith('request-code')
+      expect(req.session.lsjSmokeTestUser).toBeFalsy()
+    })
+
+    it('should redirect to request code page with errors given service fails', async () => {
+      req.body = { email: 'someone@aarvark.com.cjsm.net' }
+      mockRequestOneTimeCodeValidator.mockReturnValue(true)
+      oneTimeCodeService.requestOneTimeCode.mockRejectedValue({ status: 503 })
+
+      await requestOneTimeCodeController.submitOneTimeCodeRequest(
+        req as undefined as Request,
+        res as undefined as Response
+      )
+
+      expect(oneTimeCodeService.requestOneTimeCode).toHaveBeenCalledWith(
+        'someone@aarvark.com.cjsm.net',
+        '12345678',
+        '127.0.0.1'
+      )
+      expect(res.redirect).toHaveBeenCalledWith('request-code')
+      expect(req.flash).toHaveBeenCalledWith('errors', [
+        {
+          href: '#email',
+          text: 'There was an error generating your sign in code. Try again to request a new one to sign in.',
+        },
+      ])
+      expect(req.session.lsjSmokeTestUser).toBeFalsy()
+    })
+  })
+})
---
+++ server/services/one-time-code-auth/OneTimeCodeService.test.ts
@@ -0,0 +1,46 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import nock from 'nock'
+import HmppsAuthClient from '../../data/hmppsAuthClient'
+import config from '../../config'
+import TokenStore from '../../data/cache/TokenStore'
+import OneTimeCodeService from './OneTimeCodeService'
+
+jest.mock('../../data/hmppsAuthClient')
+
+describe('One Time Code Service', () => {
+  let hmppsAuthClient: jest.Mocked<HmppsAuthClient>
+  let oneTimeCodeService: OneTimeCodeService
+  let mockedSendLegalMailApi: nock.Scope
+
+  beforeEach(() => {
+    mockedSendLegalMailApi = nock(config.apis.sendLegalMail.url)
+
+    hmppsAuthClient = new HmppsAuthClient({} as TokenStore) as jest.Mocked<HmppsAuthClient>
+    hmppsAuthClient.getSystemClientToken.mockResolvedValue('a-system-client-token')
+    oneTimeCodeService = new OneTimeCodeService(hmppsAuthClient)
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  describe('requestOneTimeCode', () => {
+    it('should request a one time code', done => {
+      mockedSendLegalMailApi.post('/oneTimeCode/email', { email: 'a.b@c.com', sessionID: '12345678' }).reply(201)
+
+      oneTimeCodeService.requestOneTimeCode('a.b@c.com', '12345678', '127.0.0.1').then(() => {
+        expect(hmppsAuthClient.getSystemClientToken).toHaveBeenCalled()
+        done()
+      })
+    })
+
+    it('should fail to request a one time code given getSystemClientToken fails', done => {
+      hmppsAuthClient.getSystemClientToken.mockRejectedValue('an error getting system client token')
+
+      oneTimeCodeService.requestOneTimeCode('a.b@c.com', '12345678', '127.0.0.1').catch(error => {
+        expect(error).toBe('an error getting system client token')
+        done()
+      })
+    })
+  })
+})
