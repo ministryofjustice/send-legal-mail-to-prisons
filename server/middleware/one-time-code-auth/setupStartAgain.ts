7b9badeb075f81a261d63298d3b2800024899a4e
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-04-20 18:56:14 +0100

    SLM-235: OTC 'code no longer valid' page and associated refactoring

--- server/middleware/one-time-code-auth/setupStartAgain.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express'
-
-export default function setupStartAgain(): Router {
-  const router = express.Router()
-
-  router.get('/start-again', (req, res) => res.render('pages/one-time-code-auth/startAgain'))
-
-  return router
-}
