773a2bb6c4e026a8e37cd3f10b38e4442574551a
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-04-27 09:13:23 +0100

    SLM-152: Added generic formatDate filter and used in views (#306)

--- server/filters/formatDateTimeForResultsPageFilter.ts
+++
@@ -1,6 +0,0 @@
-import moment from 'moment'
-
-export default function formatDateTimeForResultsPageFilter(value: string): string {
-  const dateTime = moment(value)
-  return dateTime ? dateTime.format('h:mm a [on] D MMMM YYYY') : null
-}
