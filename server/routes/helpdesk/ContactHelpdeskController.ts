5c4c4f01e94917a5ccd68e369a7b0b120e1d0e88
Nathan Russell <nathan.russell@digital.justice.gov.uk>
2022-04-29 13:04:49 +0100

    SLM-232: Removed code to support file uploads

--- server/routes/helpdesk/ContactHelpdeskController.ts
+++ server/routes/helpdesk/ContactHelpdeskController.ts
@@ -1,9 +1,7 @@
-import fs from 'fs'
 import { Request, Response } from 'express'
 import type { ContactHelpdeskForm } from 'forms'
 import ContactHelpdeskView from './ContactHelpdeskView'
 import validate from './contactHelpdeskFormValidator'
-import logger from '../../../logger'
 import ZendeskService from '../../services/helpdesk/ZendeskService'
 
 export default class ContactHelpdeskController {
@@ -20,7 +18,7 @@ export default class ContactHelpdeskController {
 
   async submitContactHelpdesk(req: Request, res: Response): Promise<void> {
     req.session.contactHelpdeskForm = { ...req.body }
-    const errors = validate(req.session.contactHelpdeskForm, req.file)
+    const errors = validate(req.session.contactHelpdeskForm)
     if (errors.length > 0) {
       req.flash('errors', errors)
       // Redirect to original url inc. query string because Legal Sender and Mail Room journeys have different urls, so can't hardcode a redirect url here
@@ -43,12 +41,6 @@ export default class ContactHelpdeskController {
       req.flash('errors', [{ text: 'There was a problem sending your message to the Helpdesk.' }])
       // Redirect to original url inc. query string because Legal Sender and Mail Room journeys have different urls, so can't hardcode a redirect url here
       return res.redirect(req.originalUrl)
-    } finally {
-      // Uploaded file properties are available in req.file; remember to delete req.file.path
-      if (req.file) {
-        logger.info(`Uploaded file: ${JSON.stringify(req.file)}`)
-        fs.unlinkSync(req.file.path)
-      }
     }
 
     req.session.contactHelpdeskForm = undefined
