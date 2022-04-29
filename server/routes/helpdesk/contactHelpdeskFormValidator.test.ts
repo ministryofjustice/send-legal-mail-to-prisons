f2bac122f6b111e66cff4b3378a1dc8388021677
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2022-04-29 13:36:58 +0100

    SLM-232: Removed code to support file uploads (#313)

--- server/routes/helpdesk/contactHelpdeskFormValidator.test.ts
+++ server/routes/helpdesk/contactHelpdeskFormValidator.test.ts
@@ -2,24 +2,20 @@ import type { ContactHelpdeskForm } from 'forms'
 import validateName from './validators/nameValidator'
 import validateEmail from './validators/emailValidator'
 import validateProblemDetail from './validators/problemDetailValidator'
-import validateUpload from './validators/uploadValidator'
 import validate from './contactHelpdeskFormValidator'
 
 jest.mock('./validators/nameValidator')
 jest.mock('./validators/emailValidator')
-jest.mock('./validators/uploadValidator')
 jest.mock('./validators/problemDetailValidator')
 
 describe('contactHelpdeskFormValidator', () => {
   let mockValidateName: jest.MockedFunction<typeof validateName>
   let mockValidateEmail: jest.MockedFunction<typeof validateEmail>
-  let mockValidateUpload: jest.MockedFunction<typeof validateUpload>
   let mockValidateProblemDetail: jest.MockedFunction<typeof validateProblemDetail>
 
   beforeEach(() => {
     mockValidateName = validateName as jest.MockedFunction<typeof validateName>
     mockValidateEmail = validateEmail as jest.MockedFunction<typeof validateEmail>
-    mockValidateUpload = validateUpload as jest.MockedFunction<typeof validateUpload>
     mockValidateProblemDetail = validateProblemDetail as jest.MockedFunction<typeof validateProblemDetail>
   })
 
@@ -30,69 +26,45 @@ describe('contactHelpdeskFormValidator', () => {
     problemDetail: 'Bad things happened',
   } as ContactHelpdeskForm
 
-  const upload = {
-    size: 1234,
-    path: '/uploads/file-abc',
-  } as Express.Multer.File
-
   it('should return no errors if valid', () => {
     mockValidateName.mockReturnValue([])
     mockValidateEmail.mockReturnValue([])
-    mockValidateUpload.mockReturnValue([])
     mockValidateProblemDetail.mockReturnValue([])
 
-    expect(validate(form, upload)).toEqual([])
+    expect(validate(form)).toEqual([])
   })
 
   it('should return name errors', () => {
     mockValidateName.mockReturnValue(['Enter a full name'])
     mockValidateEmail.mockReturnValue([])
-    mockValidateUpload.mockReturnValue([])
     mockValidateProblemDetail.mockReturnValue([])
 
-    expect(validate(form, upload)).toEqual([{ href: '#name', text: 'Enter a full name' }])
+    expect(validate(form)).toEqual([{ href: '#name', text: 'Enter a full name' }])
   })
 
   it('should return email address errors', () => {
     mockValidateName.mockReturnValue([])
     mockValidateEmail.mockReturnValue(['Enter an email address'])
-    mockValidateUpload.mockReturnValue([])
     mockValidateProblemDetail.mockReturnValue([])
 
-    expect(validate(form, upload)).toEqual([{ href: '#email', text: 'Enter an email address' }])
-  })
-
-  it('should return file upload errors', () => {
-    mockValidateName.mockReturnValue([])
-    mockValidateEmail.mockReturnValue([])
-    mockValidateUpload.mockReturnValue(['The selected file must be smaller than 20MB'])
-    mockValidateProblemDetail.mockReturnValue([])
-
-    expect(validate(form, upload)).toEqual([
-      { href: '#screenshot', text: 'The selected file must be smaller than 20MB' },
-    ])
+    expect(validate(form)).toEqual([{ href: '#email', text: 'Enter an email address' }])
   })
 
   it('should return problem detail errors', () => {
     mockValidateName.mockReturnValue([])
     mockValidateEmail.mockReturnValue([])
-    mockValidateUpload.mockReturnValue([])
     mockValidateProblemDetail.mockReturnValue(['Enter details of the problem you experienced'])
 
-    expect(validate(form, upload)).toEqual([
-      { href: '#problemDetail', text: 'Enter details of the problem you experienced' },
-    ])
+    expect(validate(form)).toEqual([{ href: '#problemDetail', text: 'Enter details of the problem you experienced' }])
   })
 
   it('should return all errors', () => {
     mockValidateName.mockReturnValue(['Enter a full name'])
     mockValidateEmail.mockReturnValue(['Enter an email address'])
-    mockValidateUpload.mockReturnValue(['The selected file must be smaller than 20MB'])
     mockValidateProblemDetail.mockReturnValue(['Enter details of the problem you experienced'])
 
-    expect(validate(form, upload)).toEqual([
+    expect(validate(form)).toEqual([
       { href: '#problemDetail', text: 'Enter details of the problem you experienced' },
-      { href: '#screenshot', text: 'The selected file must be smaller than 20MB' },
       { href: '#name', text: 'Enter a full name' },
       { href: '#email', text: 'Enter an email address' },
     ])
