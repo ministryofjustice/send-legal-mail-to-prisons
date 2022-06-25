047552e51a42a0e72372eb3197fac58b376d1318
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2022-06-25 10:00:15 +0100

    Improve error handling in scan barcode routes
    Add async middleware to ensure unexpected API responses are ultimately
    passed to error handler.

--- server/middleware/scan/setupScanBarcode.ts
+++ server/middleware/scan/setupScanBarcode.ts
@@ -1,10 +1,11 @@
-import express, { Router } from 'express'
+import express, { RequestHandler, Router } from 'express'
 import authorisationMiddleware from '../authorisationMiddleware'
 import ScanBarcodeController from '../../routes/scan/ScanBarcodeController'
 import ScanBarcodeService from '../../services/scan/ScanBarcodeService'
 import AppInsightsService from '../../services/AppInsightsService'
 import VerifyBarcodeErrorResponseMapper from '../../routes/scan/VerifyBarcodeErrorResponseMapper'
 import PrisonService from '../../services/prison/PrisonService'
+import asyncMiddleware from '../asyncMiddleware'
 
 export default function setupScanBarcode(
   scanBarcodeService: ScanBarcodeService,
@@ -13,6 +14,9 @@ export default function setupScanBarcode(
 ): Router {
   const router = express.Router()
 
+  const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+  const post = (path: string, handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
+
   const scanBarcodeController = new ScanBarcodeController(
     scanBarcodeService,
     new VerifyBarcodeErrorResponseMapper(prisonService),
@@ -20,17 +24,15 @@ export default function setupScanBarcode(
   )
 
   router.use('/scan-barcode', authorisationMiddleware(['ROLE_SLM_SCAN_BARCODE']))
-  router.get('/scan-barcode', (req, res) => scanBarcodeController.getScanBarcodeView(req, res))
-  router.post('/scan-barcode', (req, res) => scanBarcodeController.submitScannedBarcode(req, res))
-  router.get('/scan-barcode/result', (req, res) => scanBarcodeController.getScanBarcodeResultView(req, res))
-  router.get('/scan-barcode/problem', (req, res) => scanBarcodeController.getBarcodeScanProblemView(req, res))
-  router.get('/scan-barcode/further-checks-needed', (req, res) =>
-    scanBarcodeController.getFurtherChecksNeededView(req, res)
-  )
+  get('/scan-barcode', (req, res) => scanBarcodeController.getScanBarcodeView(req, res))
+  post('/scan-barcode', (req, res) => scanBarcodeController.submitScannedBarcode(req, res))
+  get('/scan-barcode/result', (req, res) => scanBarcodeController.getScanBarcodeResultView(req, res))
+  get('/scan-barcode/problem', (req, res) => scanBarcodeController.getBarcodeScanProblemView(req, res))
+  get('/scan-barcode/further-checks-needed', (req, res) => scanBarcodeController.getFurtherChecksNeededView(req, res))
 
   router.use('/manually-enter-barcode', authorisationMiddleware(['ROLE_SLM_SCAN_BARCODE']))
-  router.get('/manually-enter-barcode', (req, res) => scanBarcodeController.getManualBarcodeEntryView(req, res))
-  router.post('/manually-enter-barcode', (req, res) => scanBarcodeController.submitManualBarcode(req, res))
+  get('/manually-enter-barcode', (req, res) => scanBarcodeController.getManualBarcodeEntryView(req, res))
+  post('/manually-enter-barcode', (req, res) => scanBarcodeController.submitManualBarcode(req, res))
 
   return router
 }
