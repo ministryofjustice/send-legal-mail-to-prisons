dc94ee4cca9889f20c12a1f305aa57456ff0b6e4
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2023-11-03 08:48:24 +0000

    Move to node 20

--- server/middleware/populateCurrentUserRoles.ts
+++ server/middleware/populateCurrentUserRoles.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express'
-import jwtDecode from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 
 export default function populateCurrentUserRoles(): RequestHandler {
   return async (req, res, next) => {
