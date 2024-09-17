5035c156f4aa75cd373a165c6ecea0a2f1265357
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2024-09-17 16:10:44 +0100

    Upgrade to redis 4

--- server/middleware/setUpWebSession.ts
+++ server/middleware/setUpWebSession.ts
@@ -1,22 +1,15 @@
 import { v4 as uuidv4 } from 'uuid'
-import redis from 'redis'
 import session from 'express-session'
-import connectRedis from 'connect-redis'
+import RedisStore from 'connect-redis'
 import express, { Router } from 'express'
-import flash from 'connect-flash'
-
+import { createRedisClient } from '../data/redisClient'
 import config from '../config'
-
-const RedisStore = connectRedis(session)
-
-const client = redis.createClient({
-  port: config.redis.port,
-  password: config.redis.password,
-  host: config.redis.host,
-  tls: config.redis.tls_enabled === 'true' ? {} : false,
-})
+import logger from '../../logger'
 
 export default function setUpWebSession(): Router {
+  const client = createRedisClient()
+  client.connect().catch((err: Error) => logger.error(`Error connecting to Redis`, err))
+
   const router = express.Router()
   router.use(
     session({
@@ -26,7 +19,7 @@ export default function setUpWebSession(): Router {
       resave: false, // redis implements touch so shouldn't need this
       saveUninitialized: false,
       rolling: true,
-    })
+    }),
   )
 
   // Update a value in the cookie so that the set-cookie will be sent.
@@ -46,7 +39,6 @@ export default function setUpWebSession(): Router {
 
     next()
   })
-  router.use(flash())
 
   return router
 }
