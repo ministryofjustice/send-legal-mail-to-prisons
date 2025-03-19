8b6281fe55be57cac241631db5eb76394b9d536e
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2025-03-19 13:47:54 +0000

    Replace csurf dependency (#560)
    * Replace csurf dependency
    
    * Remove unused types
    
    * Fix where we put csrf token for form POST
    
    * Remove console.log

--- server/middleware/setupCsrf.ts
+++ server/middleware/setupCsrf.ts
@@ -1,13 +1,21 @@
 import express, { Router } from 'express'
-import csurf from 'csurf'
+import { csrfSync } from 'csrf-sync'
 
 const testMode = process.env.NODE_ENV === 'test'
 
-export default function setupCsfr(): Router {
+export default function setupCsrf(): Router {
   const router = express.Router()
 
   if (!testMode) {
-    router.use(csurf())
+    const {
+      csrfSynchronisedProtection, // This is the default CSRF protection middleware.
+    } = csrfSync({
+      // By default, csrf-sync uses x-csrf-token header, but we use the token in forms and send it in the request body, so change getTokenFromRequest so it grabs from there
+      // eslint-disable-next-line no-underscore-dangle
+      getTokenFromRequest: req => req.body._csrf,
+    })
+
+    router.use(csrfSynchronisedProtection)
   }
 
   router.use((req, res, next) => {
