e30b18f2fa4da264dc79e4c2d159b616cc5aaf51
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-12-09 10:47:43 +0000

    More fixing

--- smoke_tests/run-smoke-test.sh
+++ smoke_tests/run-smoke-test.sh
@@ -46,7 +46,7 @@ DIR=/tmp/slm-smoke-test-$ENV
 rm -rfv $DIR 2>/dev/null
 mkdir -p $DIR
 cp -r ../integration_tests/* $DIR
-cp cypress.config.ts package.json package-lock.json .npmrc reporter-config.json run-smoke-test.sh $DIR
+cp cypress.config.ts package.json package-lock.json .npmrc .nvmrc reporter-config.json run-smoke-test.sh $DIR
 cd $DIR
 npm run setup
 
