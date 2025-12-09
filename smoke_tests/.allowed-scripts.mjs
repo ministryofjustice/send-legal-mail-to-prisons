e1d73d56d9f10d048dccf993685418de6e4ece82
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-12-09 10:45:46 +0000

    Try fixing smoke tests

---
+++ smoke_tests/.allowed-scripts.mjs
@@ -0,0 +1,7 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+  allowlist: {
+    'node_modules/cypress@15.7.1': 'ALLOW',
+  },
+})
