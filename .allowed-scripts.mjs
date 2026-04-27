735194c7f2fe52a120c2d2bd457793a85decc888
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2026-04-27 08:20:04 +0100

    Update .allowed-scripts.mjs

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,7 +4,7 @@ export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     'node_modules/canvas@3.2.3': 'ALLOW',
-    'node_modules/cypress@15.14.0': 'ALLOW',
+    'node_modules/cypress@^15.14.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
