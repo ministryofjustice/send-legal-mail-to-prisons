d593bdc45afaa63f8cb11da2c2f6c1beca5fece0
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-07-28 10:15:03 +0100

    Update dependencies 2026-07-28

--- smoke_tests/.allowed-scripts.mjs
+++ smoke_tests/.allowed-scripts.mjs
@@ -2,6 +2,6 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/cypress@15.18.1': 'ALLOW',
+    'node_modules/cypress@15.19.0': 'ALLOW',
   },
 })
